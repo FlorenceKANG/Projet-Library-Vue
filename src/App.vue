0d966c283959d9e608442f3110c84296245337cc
Florence KANG <fkang@outlook.fr>
2026-02-13 11:56:41 +0100

    feat: added raw search bar styled

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container">
+  <header class="container">
     <img src="./assets/logo.png" alt="Logo du site" />
     <h1>Bienvenue Chez Vue Library !</h1>
-  </div>
+  </header>
+
+  <main>
+    <div class="search">
+      <label for="search" class="field border prefix search-input">
+        <i>search</i>
+        <input id="search" type="text" placeholder="Chercher l'oeuvre souhaité" />
+      </label>
+      <button class="small-round large search-btn">Rechercher</button>
+    </div>
+  </main>
 </template>
 
 <style>
@@ -16,5 +26,18 @@ h1 {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 0.5rem;
+  margin: 1rem;
+}
+.search {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 0.5rem;
+}
+.search-input {
+  grid-area: span 4 / span 4;
+}
+.search-btn {
+  grid-column-start: 5;
 }
 </style>
