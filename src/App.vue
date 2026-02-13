927561f5f551d59468dcfaad5b6df42a5a219fd9
Florence KANG <fkang@outlook.fr>
2026-02-13 11:02:30 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <img src="./assets/logo.png" alt="Logo du site" />
+    <h1>Bienvenue Chez Vue Library !</h1>
+  </div>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
